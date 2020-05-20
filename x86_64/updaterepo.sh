f412d346727b201671ed761c9e07df7cffdbd84d
crborga <chrisborga@yahoo.com>
2020-05-20 00:36:28 -0400

    .

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -10,7 +10,10 @@ rm *.files.tar.gz
 echo "repo-add"
 repo-add -n -R hefftylionos.db.tar.gz *.pkg.tar.zst
 #sleep 8
+rm hefftylionos.db
+rm hefftylionos.files
 cp -f hefftylionos.db.tar.gz hefftylionos.db
+cp -f hefftylionos.files.tar.gz hefftylionos.files
 
 echo "####################################"
 echo "Repo Updated!!"
