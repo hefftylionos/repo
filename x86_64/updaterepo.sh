c7b57fe25659135c260c621e00ad63eec818f8a5
crborga <chrisborga@yahoo.com>
2020-05-21 09:32:52 -0400

    package fixes

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-rm *.db
-rm *.files
-rm *.db.tar.gz
-rm *.files.tar.gz
-
-
+rm hefftylionos.db
+rm hefftylionos.files
+rm hefftylionos.db.tar.gz
+rm hefftylionos.files.tar.gz
 
 echo "repo-add"
-repo-add -n -R hefftylionos.db.tar.gz *.pkg.tar.zst
-#sleep 8
+repo-add -s -n -R hefftylionos.db.tar.gz *.pkg.tar.zst
+sleep 8
 rm hefftylionos.db
 rm hefftylionos.files
 cp -f hefftylionos.db.tar.gz hefftylionos.db
