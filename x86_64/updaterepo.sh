a274cf48b1cbb60215ee16889d406cbb41ed34e0
crborga <chrisborga@yahoo.com>
2020-05-21 10:00:29 -0400

    fix arcokeyring pkg

--- x86_64/updaterepo.sh
+++ x86_64/updaterepo.sh
@@ -6,12 +6,14 @@ rm hefftylionos.db.tar.gz
 rm hefftylionos.files.tar.gz
 
 echo "repo-add"
-repo-add -s -n -R hefftylionos.db.tar.gz *.pkg.tar.zst
+repo-add -n -R hefftylionos.db.tar.gz *.pkg.tar.zst
 sleep 8
 rm hefftylionos.db
 rm hefftylionos.files
 cp -f hefftylionos.db.tar.gz hefftylionos.db
 cp -f hefftylionos.files.tar.gz hefftylionos.files
+sudo rm /repo/x86_64/*
+sudo cp -r . /repo/x86_64/
 
 echo "####################################"
 echo "Repo Updated!!"
